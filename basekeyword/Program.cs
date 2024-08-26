a44a8a28821a5c2e9bc8bc604f6ad64ce3ba25ce
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-08-26 14:39:29 +0330

    examples for base keyword

---
+++ basekeyword/Program.cs
@@ -0,0 +1,36 @@
+﻿// The base keyword is used to access members of the base class from within a derived class.
+// Use it if you want to:
+// Call a method on the base class that has been overridden by another method.
+// Specify which base-class constructor should be called when creating instances of the derived class.
+
+// Example 1
+public class Person
+{
+    protected string ssn = "444-55-6666";
+    protected string name = "John L. Malgraine";
+
+    public virtual void GetInfo()
+    {
+        Console.WriteLine("Name: {0}", name);
+        Console.WriteLine("SSN: {0}", ssn);
+    }
+}
+class Employee : Person
+{
+    public string id = "ABC567EFG";
+    public override void GetInfo()
+    {
+        // Calling the base class GetInfo method:
+        base.GetInfo();
+        Console.WriteLine("Employee ID: {0}", id);
+    }
+}
+
+class TestClass
+{
+    static void Main()
+    {
+        Employee E = new Employee();
+        E.GetInfo();
+    }
+}
---
+++ basekeyword_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// The base keyword is used to access members of the base class from within a derived class.
+// Use it if you want to:
+// Call a method on the base class that has been overridden by another method.
+// Specify which base-class constructor should be called when creating instances of the derived class.
+
+// Example 2
+public class BaseClass
+{
+    int num;
+
+    public BaseClass()
+    {
+        Console.WriteLine("in BaseClass()");
+    }
+
+    public BaseClass(int i)
+    {
+        num = i;
+        Console.WriteLine("in BaseClass(int i)");
+    }
+
+    public int GetNum()
+    {
+        return num;
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    // This constructor will call BaseClass.BaseClass()
+    public DerivedClass() : base()
+    {
+    }
+
+    // This constructor will call BaseClass.BaseClass(int i)
+    public DerivedClass(int i) : base(i)
+    {
+    }
+
+    static void Main()
+    {
+        DerivedClass md = new DerivedClass();
+        DerivedClass md1 = new DerivedClass(1);
+    }
+}
